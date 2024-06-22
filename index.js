325808477f0b3d3d9753e4535f81147d70c18b4a
Aditya Bhargava <adit@adit.io>
2024-06-22 17:14:43 -0500

    unneeded

--- index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const tarsec_1 = require("tarsec");
-// define a parser
-const parser = (0, tarsec_1.seqR)((0, tarsec_1.str)("hello"), tarsec_1.space, (0, tarsec_1.str)("world"));
-// then use it
-parser("hello world"); // success
-parser("hello there"); // failure
